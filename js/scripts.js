64969c91f85b512b780eed00d15022ce0ad153df
apul8377 <apulvers@my.chemeketa.edu>
2020-07-01 00:01:52 -0700

    added a jquery function to the program

--- js/scripts.js
+++ js/scripts.js
@@ -2,26 +2,30 @@ function CustomerId() {
   (this.customerInfo = []), (this.currentId = 0);
 }
 
-CustomerId.prototype.addCustomer = function (customer)({
+CustomerId.prototype.addCustomer = function (customer) {
   customer.id = this.assignId();
-  this.customerInfo.push(customer),
-})
+  this.customerInfo.push(customer);
+};
 
 CustomerId.prototype.assignId = function () {
   this.currentId += 1;
   return this.currentId;
+};
+function Customer(userName, passWord) {
+  (this.userName = userName), (this.passWord = passWord);
 }
 
-  
-  //UI logic~~~~~~~~~~~~~~~~~~~~
-  
-  CustomerId.prototype.getCustomerInfo()
-{
-  let username = document.getElementById("username").value
-  let password = document.getElementId("password").value
- 
+//UI logic~~~~~~~~~~~~~~~~~~~~
+let customerId = new CustomerId();
+$(document).ready(function () {
+  $("form#customer-info").submit(function (event) {
+    event.preventDefault();
+
+    const userName = $("input#username").val();
+    const passWord = $("input#password").val();
+
     console.log("input received, validating....");
-  let customer = [];
-  customer.push(username, password);
-  return customer;
-}
+    let newCustomer = new Customer(userName, passWord);
+    customerId.addCustomer(newCustomer);
+  });
+});
