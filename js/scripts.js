c5c8058cd8bbc00a605fd485a782f813b5af7a37
apul8377 <apulvers@my.chemeketa.edu>
2020-07-02 01:44:29 -0700

    functioning username storage in the customerDb constructor

--- js/scripts.js
+++ js/scripts.js
@@ -1,29 +1,35 @@
 // //~~~~~~~~Business Logic~~~~~~~~~~
+//The constructor to store our customer order history
+function CustomerDb() {
+  this.customers = [];
+}
+
+CustomerDb.prototype.storeCustomers = function (customer) {
+  this.customers.push(customer);
+};
 
 //The constructor to build a customer with an associated id/order number
-function CustomerDb() {
-  (this.name = []), (this.orderNumber = 0);
+function Customer(name) {
+  this.name = name;
+  //this.orderNumber = 0;
 }
 
 //This is where we will increment an order number by 1 to associate with each new order
-CustomerDb.prototype.assignOrderNumber = function () {
+Customer.prototype.assignOrderNumber = function () {
   this.orderNumber += 1;
   return this.orderNumber;
 };
 
-CustomerDb.prototype.storeCustomers = function (customer) {};
-
 //~~~~~~~~~~~~UI logic~~~~~~~~~~~~
 //This takes the users name from the html form and stores it in username. Then returns username
+let customerDb = new CustomerDb();
 
 $("document").ready(function () {
   $("form#customer-info").submit(function (event) {
     event.preventDefault();
     let name = $("input#username").val();
-    let newName = name;
     // let newNumber = assignOrderNumber();
-    let newCustomer = new Customer(name, newNumber);
-    storeCustomers(newCustomer);
-    console.log(newCustomer);
+    let customer = new Customer(name);
+    customerDb.storeCustomers(customer);
   });
 });
